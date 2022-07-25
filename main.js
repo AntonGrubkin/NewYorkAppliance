3843146d1abefebdc062a6370646747656cd3029
AntonGrubkin <elcomendante86@gmail.com>
2022-07-25 20:36:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+
+/*const time = 3500;
+const step = 21;
+
+function outNum(num, elem) {
+    let l = document.querySelector('#' + elem);
+    n = 0;
+    let t = Math.round(time / (num / step));
+    let interval = setInterval(() => {
+        n = n + step;
+        if (n >= num) {
+            clearInterval(interval);
+        }
+        l.innerHTML = n;
+    },
+         t);
+}
+
+outNum(4648, 'out');
+outNum(5761, 'out-2');
+outNum(741, 'out-3');
+outNum(3648, 'out-4');
+outNum(2648, 'out-5');
+outNum(511, 'out-6');
+outNum(2918, 'out-7');
+outNum(5319, 'out-8');
+*/
+
+let time = 2, cc = 1;
+
+$(window).scroll(function(){
+    $('#infoBlock').each(function(){
+        let
+        cPos = $(this).offset().top,
+        topWindow = $(window).scrollTop();
+        if (cPos < topWindow + 500) {
+            if (cc < 2) {
+                $('.out').addClass('viz');
+                $('div').each(function(){
+                    let
+                    i = 500,
+                    num = $(this).data('num'),
+                    step = 1000 * time / num,
+                    that = $(this),
+                    int = setInterval(function() {
+                        if (i <= num) {
+                            that.html(i);
+                        }
+                        else {
+                            cc = cc + 2;
+                            clearInterval(int);
+                        }
+                        i++
+                    },step);
+                });
+            }
+        }
+
+    });
+});
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('#block');
+  
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
